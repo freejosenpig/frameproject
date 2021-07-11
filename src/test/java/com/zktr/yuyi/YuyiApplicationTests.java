e88c63a09aac5cc7cc52a14cff0666ae0310fe63
DESKTOP-PD78V0F\宇义 <957176817@qq.com>
2021-07-11 08:33:59 +0800

    首次提交

---
+++ src/test/java/com/zktr/yuyi/YuyiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zktr.yuyi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuyiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
